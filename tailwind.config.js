9f793ec9b1fa0a20363247042528155141ccce34
Achmad Tirto Sudiro <achmadtirtosudirosudiro@gmail.com>
2024-11-30 21:06:54 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+import daisyui from 'daisyui';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: '#FAF9FE',
+        blue: '#36B4E5',
+        primary1: '#05445E',
+        success: '#00FF6E',
+        error: '#D92525',
+      },
+      fontFamily: {
+        sans: ['"Nunito Sans"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#05445E',     // Use primary1 color
+          secondary: '#36B4E5',   // Use blue color
+          accent: '#F2F3F3',      // Use background color
+          neutral: '#3D4451',
+          success: '#00FF6E',
+          error: '#D92525',
+          'base-100': '#FFFFFF',
+        },
+      },
+    ],
+  },
+};
